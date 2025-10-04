c51a4a449237553c3fadd8e4340d68f733e39122
assettasimone <assettasimone@gmail.com>
2025-10-04 19:43:15 +0200

    Refactor App component to enhance layout and functionality with language selection

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,39 @@ import { useState } from 'react'
 import languages from './data/languages.js'
 
 function App() {
+  const [currentLang, setCurrentLang] = useState(1)
+
+
 
 
   return (
     <>
-      {languages.map(item => (
-        <button key={item.id}>{item.title}</button>
+      <div className="container ">
+        <header className='m-4 text-center'>
+          <h1 className='mb-4'>Learn web Development</h1>
 
-      ))}
+          {languages.map(item => (
+            <button className='btn btn-primary me-2' key={item.id} onClick={() => setCurrentLang(item.id)} >{item.title}</button>
+          ))}
+        </header>
+        <main className=''>
+          {languages.map(item => (
 
+            currentLang === item.id && (
+              <div key={item.id} className="card col-5 m-auto">
+                <div className="card-header">
+                  <h3 className='card-title'>{item.title}</h3>
+                </div>
+                <div class="card-body">
+
+                  <p className='card-text'>{item.description}</p>
+                </div>
+              </div>
+
+            )
+          ))}
+        </main>
+      </div>
     </>
   )
 }
